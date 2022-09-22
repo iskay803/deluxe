0f717c884913de8c2edd59b62c3928218598e532
iskay <iskay803@gmail.com>
2022-09-22 23:48:53 +0300

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".process-slider", {
+    slidesPerView: "auto",
+    spaceBetween: 63,
+    breakpoints: {
+        768: {
+            spaceBetween: 88,
+        }
+    }
+});
+
+
+console.log(swiper)

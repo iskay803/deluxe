75bd2d13beeb0291e1dbdeaa5ab4ba11ee102865
iskay <iskay803@gmail.com>
2022-09-29 23:34:50 +0300

    animation

--- script.js
+++ script.js
@@ -8,5 +8,43 @@ var swiper = new Swiper(".process-slider", {
     }
 });
 
+new WOW().init();
 
-console.log(swiper)
+addEventListener('scroll', (event) => {
+    const targetEl = document.querySelector('.our-success-block');
+    const rect = targetEl.getBoundingClientRect();
+
+    if (targetEl.classList.contains('calculated')) {
+        return;
+    }
+
+    if (rect.top < 0 ) {
+        return;
+    }
+
+    if (rect.bottom >= window.innerHeight) {
+        return;
+    }
+
+    targetEl.classList.add('calculated');
+
+    function startCounter(el) {
+        const maxValue = el.textContent;
+        let i = 0;
+
+        const iteration = function () {
+            setTimeout(function () {
+                el.textContent = i++;
+
+                if (i <= maxValue) {
+                    iteration();
+                }
+            }, 100)
+        }
+        iteration();
+    }
+
+    targetEl.querySelectorAll('.my-counter').forEach(function (el) {
+        startCounter(el);
+    });
+});
